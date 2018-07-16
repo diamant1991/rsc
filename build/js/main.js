b2de98d632edb66f14ce8e72b04fd5b7b8382010
diamant1991 <diamant1988@mail.ru>
2018-07-16 15:17:28 +0300

    first files

---
+++ build/js/main.js
@@ -0,0 +1,41 @@
+webpackJsonp([0],{
+
+/***/ 125:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(126);
+
+/***/ }),
+
+/***/ 126:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(127);
+
+var _svg4everybody = __webpack_require__(329);
+
+var _svg4everybody2 = _interopRequireDefault(_svg4everybody);
+
+var _jquery = __webpack_require__(330);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _svg4everybody2.default)();
+
+window.$ = _jquery2.default;
+window.jQuery = _jquery2.default;
+
+__webpack_require__(331);
+
+/***/ })
+
+},[125]);
+//# sourceMappingURL=main.js.map
