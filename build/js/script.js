8e35b6dc0661818ff3109ef8143f8d3e95d38a94
diamant1991 <diamant1988@mail.ru>
2018-07-29 20:48:30 +0300

    иконки, страница подкатегории

--- build/js/script.js
+++ build/js/script.js
@@ -25,3 +25,27 @@ if(window.matchMedia('(max-width: 991px)').matches){
 $('.slider').slick({
   dots: false
 });
+
+$('.product-carousel').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: $('.prev-btn'),
+  nextArrow: $('.next-btn'),
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
